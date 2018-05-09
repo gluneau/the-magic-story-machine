febef66dfa5caeeaed02a30039b3b480ebfb3780
mktcode <kontakt@markus-kottlaender.de>
2018-05-09 16:56:40 +0200

    added more verbose logs

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-// TODO: add more verbose logs
 // TODO: automatic reward distribution
 // TODO: multilanguage support
 
@@ -10,6 +9,7 @@ if (!helper.BOT_ACCOUNT_NAME || !helper.BOT_KEY || !helper.BOT_TAGS) {
 }
 
 (async () => {
+  console.log('Fetching data...');
   // get data: all posts from bot account and all comments from latest story post
   let posts = await helper.getPosts();
   let comments = [];
@@ -23,6 +23,7 @@ if (!helper.BOT_ACCOUNT_NAME || !helper.BOT_KEY || !helper.BOT_TAGS) {
       meta.hasOwnProperty('toBeContinued')
     ) {
       comments = await helper.getComments(posts[i].permlink);
+      console.log('Found ' + posts.length + ' posts and ' + comments.length + ' comments for latest post.');
       break;
     }
   }
@@ -40,8 +41,14 @@ if (!helper.BOT_ACCOUNT_NAME || !helper.BOT_KEY || !helper.BOT_TAGS) {
     const intro = helper.getPostIntro(pot);
     const footer = helper.getPostFooter();
 
+    console.log('Found ' + currentStoryPosts.length + ' posts in current story.');
+    console.log('Found ' + validComments.length + ' valid commands for latest story post.');
+    console.log('Most upvoted command: ' + JSON.stringify(command));
+    console.log('Pot value: ' + pot);
+
     if (storyHasEnded) {
       // start new story
+      console.log('Story has ended. Starting a new one...');
       helper.post(
         intro + '\n\n# ' + lastPostMeta.startPhrase + '\n# \n\n## ' + lastPostMeta.toBeContinued + '\n\n' + footer,
         lastPostMeta,
@@ -56,6 +63,7 @@ if (!helper.BOT_ACCOUNT_NAME || !helper.BOT_KEY || !helper.BOT_TAGS) {
 
       if (command.type === 'end') {
         // publish last story post
+        console.log('Story will end. Publishing last post...');
         helper.post(
           intro + '\n\n# ' + lastPostMeta.startPhrase + '\n# \n\n' + storyBody + ' \n\n' + footer,
           lastPostMeta,
@@ -64,6 +72,7 @@ if (!helper.BOT_ACCOUNT_NAME || !helper.BOT_KEY || !helper.BOT_TAGS) {
         );
       } else if (command.type === 'append') {
         // publish next story post
+        console.log('Story goes on. Publishing next post...');
         helper.post(
           intro + '\n\n# ' + lastPostMeta.startPhrase + '\n# \n\n' + storyBody + ' \n\n## ' + lastPostMeta.toBeContinued + footer,
           lastPostMeta,
@@ -75,9 +84,11 @@ if (!helper.BOT_ACCOUNT_NAME || !helper.BOT_KEY || !helper.BOT_TAGS) {
 
     // upvote comments
     if (validComments.length) {
+      console.log('Upvoting commands:');
       let weight = 10000; // 100% for the first one
       validComments.forEach(comment => {
         setTimeout(() => {
+          console.log('Upvoting @' + comment.author + '/' + comment.permlink);
           helper.upvote(comment, weight);
           weight = 1000; // 10% for all others
         }, 5000);
